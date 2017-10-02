dff39363615704bf1deae5af0175dd58179019cd
Dominic Chapman <dominicchapman@me.com>
2017-10-02 21:41:52 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 def countdown(seconds_until_new_year)
   while seconds_until_new_year > 0
     return "#{seconds_until_new_year} SECOND(S)!"
-    seconds_until_new_year =- 1
+    seconds_until_new_year -= 1
   end
   return "HAPPY NEW YEAR!"
 end
