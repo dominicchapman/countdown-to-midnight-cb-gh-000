8f358e201580a3b03b7e1f028f26655e2146bb0f
Dominic Chapman <dominicchapman@me.com>
2017-10-02 21:44:38 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,17 @@
 #write your code here
 def countdown(seconds_until_new_year)
   while seconds_until_new_year > 0
-    puts "#{seconds_until_new_year} SECOND(S)!"
+    return "#{seconds_until_new_year} SECOND(S)!"
     seconds_until_new_year -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_until_new_year)
+  while seconds_until_new_year > 0
+    return "#{seconds_until_new_year} SECOND(S)!"
+    sleep(1)
+    seconds_until_new_year -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
